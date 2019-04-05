7deeaba4a51cf364391a06468626b126528f39d2
Kim <michael.kim@nuance.com>
2019-04-05 09:16:24 -0400

    Change lat/long fields to Google Maps Link, Added marker clustering

--- src/app.js
+++ src/app.js
@@ -5,16 +5,24 @@ $(document).ready(function() {
 
     /* Submit Place button */
     $("#submitPlace").on("click", function(){
+        let googleMapsLink = $("#googleMapsLink").val();
+        console.log(googleMapsLink);
+        const latitude = googleMapsLink.substring(googleMapsLink.indexOf("@") + 1, googleMapsLink.indexOf(","));
+        console.log(latitude);
+        googleMapsLink = googleMapsLink.substring(googleMapsLink.indexOf(",") + 1);
+        console.log(googleMapsLink);
+        const longitude = googleMapsLink.substring(0, googleMapsLink.indexOf(","));
+        console.log(longitude);
+
         firebase.database().ref('places').push({
             name: $("#name").val(),
             category: $("#category").val(),
-            latitude: $("#latitude").val(),
-            longitude: $("#longitude").val()
+            latitude,
+            longitude
         })
         $("#name").val("");
         $("#category").val("");
-        $("#latitude").val("");
-        $("#longitude").val("");
+        $("#googleMapsLink").val("");
 
         setTimeout(function() {
             window.location.reload();
